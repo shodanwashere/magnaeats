8532c4c8cb1704a04c3db0ab70a09848702b40c2
Nuno Dias <jookaada@protonmail.com>
2022-03-23 21:57:17 +0000

    added restaurant.c

---
+++ src/restaurant.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "memory.h"
+#include "main.h"
+
+int execute_restaurant(int rest_id, struct communication_buffers* buffers, struct main_data* data){
+  int ops_procs = 0;
+  while(true){
+    // TODO
+    
+    if(data->terminate)
+      break;
+  }
+  return ops_procs;
+}
